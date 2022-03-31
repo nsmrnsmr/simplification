082c151a251956f4bb8162737096e4d551aab817
nsmrnsmr <96044484+nsmrnsmr@users.noreply.github.com>
2022-03-31 18:50:25 +0900

    Update QEM_mcm.cpp

--- source/QEM_mcm.cpp
+++ source/QEM_mcm.cpp
@@ -12,41 +12,6 @@ using Vec3d = Eigen::Vector3d;
 using MatXd = Eigen::MatrixXd;
 using Mat3d = Eigen::Matrix3d;
 
-struct Stopwatch {
-    std::chrono::system_clock::time_point s, e;
-    std::vector<double> lap; // ラップタイム
-
-    // 再生
-    void start() {
-        s = std::chrono::system_clock::now();
-    }
-
-    // 計測終了, ミリ秒を返す
-    double stop() {
-        e = std::chrono::system_clock::now();
-        return static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count());
-    }
-
-    // 計測時間を返す(stop後でないと無意味)
-    double time() const {
-        return static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count());
-    }
-
-    // 経過時間をラップタイムとして保存 & 再スタート
-    void section() {
-        e = std::chrono::system_clock::now();
-        lap.emplace_back(static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count()));
-        s = std::move(e);
-    }
-
-    // 計測時間・ラップタイムのクリア
-    void clear() {
-        s = std::chrono::system_clock::now();
-        e = std::chrono::system_clock::now();
-        lap.clear();
-    }
-};
-
 struct Face{
   int a,b,c;
   Face(int a_=0 ,int b_=0, int c_=0): a(a_), b(b_), c(c_){}
@@ -239,9 +204,6 @@ int main(int argc, char *argv[]){
 //辺の更新をどうするか：unionfind
 //QEMの更新をどうするか：頂点に対してタイムスタンプを利用
 
-    Stopwatch time;
-  time.start();
-
   int dd=0;
   for(int cnt=0; cnt<v-n; cnt++){
     //std::cout <<cnt<< " roop\n";
@@ -290,9 +252,8 @@ int main(int argc, char *argv[]){
     //std::cout <<"cnt: "<< cnt << "\n";
     dd++;
   }
-std::cout << "time " <<time.stop()<<"\n";
-  std::cout << "remeshing\n";
-//メッシュの再構成////////////////////////////////////////////////////////
+  std::cout << "output\n";
+//メッシュの構成////////////////////////////////////////////////////////
   std::map<int,int> m;
   std::vector<Vec3d> newVertex;
   std::vector<Face> newFace;
